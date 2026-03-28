2a8873b6fe1004649b0dd6c66235e2aedf2d8591
Tim <N/A>
2026-03-28 14:11:47 +0000

    submitting js130 lesson5 ex01

---
+++ js130/lesson5/ex02.js
@@ -0,0 +1,4 @@
+/*
+In our solution for the previous problem, what do you think would happen if you replaced let delay with var delay? 
+Go ahead and try it and see if you can explain the difference in behavior.
+*/
